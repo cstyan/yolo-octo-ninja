9fff3d05530dd1b4d17b1c82e151eec9ca2cf80e
unclekevin <kevintangeman@dccnet.com>
2013-04-01 13:22:24 -0700

    format editing

--- src/client-gui.cpp
+++ src/client-gui.cpp
@@ -351,8 +351,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
       case ID_SONGS_PLAYPREV:
       case IDC_BTN_PREV:
 		indexItem = (int)SendMessage(slb, LB_GETCURSEL, 0, 0);	// gets current selectin index
-        if (indexItem != LB_ERR) {
+        
+		if (indexItem != LB_ERR) {
 		  indexItem -= 1;			// decrements it to get prev index
+		  
 		  if(indexItem >= 0){		// make sure current selection wasn't already first in list
 	  	    SendMessage(slb, LB_SETCURSEL, indexItem, 0);		// select prev song in list
 		    SendMessage(hWnd, WM_COMMAND, IDC_BTN_PLAY, 0);		// send 'play' message
@@ -363,9 +365,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
       case ID_SONGS_PLAYNEXT:
       case IDC_BTN_NEXT:
 		indexItem = (int)SendMessage(slb, LB_GETCURSEL, 0, 0); 	// gets current selection index
-        if (indexItem != LB_ERR) {
+        
+		if (indexItem != LB_ERR) {
 		  indexItem += 1;			// increments it to get next index
 		  countItem = (int)SendMessage(slb, LB_GETCOUNT, 0, 0);	// get count of items in list
+		  
 		  // make sure current selection wasn't already last in list
 		  if((countItem != LB_ERR) && (indexItem < countItem)){
 		    SendMessage(slb, LB_SETCURSEL, indexItem, 0); 		// select prev song in list
